c66e25c44fefde703e4010d0626f0b98d23eea08
Uendel Ives de Araujo <uendel.araujo@al.infnet.edu.br>
2025-08-10 12:06:42 -0400

    Added the first image and the Spring Boot project generated to start.spring.io

---
+++ math-api-springboot/src/main/java/com/example/mathapi/MathApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mathapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathApiSpringbootApplication.class, args);
+	}
+
+}
